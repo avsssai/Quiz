617357429fdb398c49eac76c7bd7b8ef6c754310
avsssai <34810623+avsssai@users.noreply.github.com>
2020-05-28 16:36:51 +0530

    made question sets, work on displaying questions

--- script.js
+++ script.js
@@ -18,9 +18,41 @@
 class Quiz {
     constructor(data){
         this.data = data;
+        this.start = false;
+        this.end = false;
+        this.round = 0;
+        this.score = 0;
+        this.makeQuestionSets();
+    };
+    makeQuestionSets() {
+        let questionSets = [...this.data.results];
+        questionSets.forEach(set => {
+            set.allAnswers = this.shuffle([set.correct_answer,...set.incorrect_answers]);
+            this.questionSets = questionSets;
+        })
+    };
+    choseIncorrectly() {
+        this.score = this.score - 1;
+    }
+    choseCorrectly() {
+        this.score = this.score + 1;
+    };
+    newGame() {
+        this.end = true;
+        this.round = 0;
+        this.score = 0;
+    }
+    shuffle(array) {
+        let arrayToShuffle = [...array];
+        // Fisher-Yates algorithm
+        for(let i=arrayToShuffle.length-1; i>0; i--){
+            let j = Math.floor(Math.random()* (i+1)); // random number between 1 to i;
+            [arrayToShuffle[i],arrayToShuffle[j]] = [arrayToShuffle[j],arrayToShuffle[i]];
+        }
+        return arrayToShuffle;
     };
 
-}
+}   
 
 async function getData() {
   try {
@@ -35,7 +67,15 @@ async function getData() {
 async function useData() {
     let data = await getData();
     let quiz = new Quiz(data);
+
+
     console.log(quiz);
 }
 
-useData();
+
+let fetchData = document.querySelector('button');
+
+fetchData.addEventListener('click',(e)=>{
+    useData();
+
+})
